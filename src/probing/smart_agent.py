0cadeb02192552409b61f3fbd4616b99af5af77e
ChristianMuenkel <christian@muenkel.cc>
2022-03-04 13:01:38 +0100

    evaluation for trained models

--- src/probing/smart_agent.py
+++ src/probing/smart_agent.py
@@ -1,6 +1,6 @@
 # https://pythonprogramming.net/saving-and-loading-reinforcement-learning-stable-baselines-3-tutorial/ TODO
 # Energy for 100k steps: 50312 J ~ 14Wh
-
+import numpy as np
 
 import src.environment.energy_management as em
 import gym
@@ -12,12 +12,13 @@ import argparse
 import math
 import time
 from src.probing.experiments import EXPERIMENTS
+import pickle
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-m", "--mode", choices=["auto", "manual"], default="manual",
                     help="Use auto in combination with a predefined experiment or manually set parameters")
-parser.add_argument('-a', '--action', choices=['train', 'show'],
-                    help='Train from scratch or visualize existing checkpoint')
+parser.add_argument('-a', '--action', choices=['train', 'show', 'eval'],
+                    help='Train from scratch or visualize existing checkpoint or evaluate results')
 parser.add_argument("-e", "--experiment", type=int, choices=list(range(11)), default=-1, help="Predefined experiment to run")
 parser.add_argument('-s', '--steps', type=int, default=100000)
 parser.add_argument("-S", "--seed", type=int, help="Seed used for the environment")
@@ -31,6 +32,7 @@ SB3_PARAMS = {
 
 
 def train():
+    """Use Stable Baselines 3 to train an agent on the environment once for every seed"""
     env = em.EnergyManagementEnv(**env_params)
     mean_rewards = []
     reward_stds = []
@@ -64,7 +66,37 @@ def train():
         print(f"Seed {s} | mean reward {round(mean, 2)} | reward std {round(std, 2)}")
 
 
+def evaluate():
+    """Evaluate the performance of a pre-trained agent"""
+    env = em.EnergyManagementEnv(**env_params)
+    mean_rewards = []
+    std_rewards = []
+    for seed in seeds:
+        env.reset(seed=seed)
+        model_path = f"{models_dir}/{seed}/{steps}.zip"
+        if algorithm == "PPO":
+            model = PPO.load(model_path, env=env)
+        elif algorithm == "DQN":
+            model = DQN.load(model_path, env=env)
+        mean_reward, std_reward = evaluate_policy(model, model.get_env(), n_eval_episodes=10)
+        mean_rewards.append(mean_reward)
+        std_rewards.append(std_reward)
+
+    mean = np.mean(mean_rewards)
+    qc = 0
+    n1 = len(mean_rewards)
+    for i in range(len(seeds)):
+        qc += (n1 - 1) * std_rewards[i]**2 + n1 * mean_rewards[i]**2
+
+    for m, s in zip(mean_rewards, std_rewards):
+        print("mean", m, "| std", s)
+    sc = np.sqrt((qc - n1*len(seeds) * mean**2) / (n1 * len(seeds) - 1))
+    print("\nEvaluation results:")
+    print(f"mean reward {round(mean, 5)} | reward std {round(sc, 5)}")
+
+
 def visualize():
+    """Visualize a pre-trained agents actions on the environment"""
     env = em.EnergyManagementEnv(**env_params)
     env.reset()  # do not seed so we can call it multiple times
 
@@ -110,9 +142,11 @@ if __name__ == '__main__':
 
     if args.action == 'train':
         train()
-    else:
+    elif args.action == "show":
         seed = args.seed if args.seed else 0
         models_dir += f"/{seed}"
         visualize()
+    else:
+        evaluate()
     time_end = time.time()
     print(f"Took {round(time_end - time_start, 1)}s to run")
